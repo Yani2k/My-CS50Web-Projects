670a8d15b57b9858e4028366e33cd2f2e0c05e16
Yani2k <yaniyankovenchev@gmail.com>
2023-03-22 16:35:49 +0200

    Adding Entry Page and Index Page.

--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -7,4 +7,15 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+    
+def entry(request, title):
+  ent = util.get_entry(title)
+  if ent is None:
+    return render(request, "encyclopedia/error_page.html", {
+      "error": "Title not found"
+    })
+  return render(request, "encyclopedia/entry.html", {
+    "title": title,
+    "body": ent
+  })
 
